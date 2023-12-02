b397e79ce41fc498cb6bc0a14b94b3a06c91da8a
Raison <raison.song@mingie.cn>
2023-12-02 19:30:16 +0800

    feature(SAPP):update email.go

--- Utils/EmailUtil/email.go
+++ Utils/EmailUtil/email.go
@@ -3,11 +3,11 @@ package EmailUtil
 import (
 	"github.com/SZCU-SNC/SzcuAcademicGPT-private-packet/Utils/ConfigUtil"
 	"github.com/jordan-wright/email"
-	"log"
 	"net/smtp"
+	"os"
 )
 
-func SendEmail(to []string, subject string, context string, filePath string) {
+func SendEmail(to []string, subject string, context string, filePath string) error {
 
 	server := ConfigUtil.GetConfigData()["email"].(map[interface{}]interface{})["server"].(string)
 	auth := ConfigUtil.GetConfigData()["email"].(map[interface{}]interface{})["auth"].(map[interface{}]interface{})
@@ -23,16 +23,26 @@ func SendEmail(to []string, subject string, context string, filePath string) {
 	e.Subject = subject
 	e.Text = []byte(context)
 
-	//副件
-	_, err := e.AttachFile(filePath)
-	if err != nil {
-		return
+	if filePath != "" {
+		_, err := os.Stat(filePath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return err // 文件不存在
+			}
+		}
+		//副件
+		_, err = e.AttachFile(filePath)
+		if err != nil {
+			return err
+		}
 	}
 
 	// 使用配置值进行发送邮件操作
-	err = e.Send(server, smtp.PlainAuth("", username, password, host))
+	err := e.Send(server, smtp.PlainAuth("", username, password, host))
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
