4b82479c929724c6f75973f4044b01b926f45853
Shanghao <90334906+shanghaox@users.noreply.github.com>
2023-12-07 10:11:30 +0800

    feat(redis): 增加重置密码的包装函数

--- Entity/RedisKey/redis_key.go
+++ Entity/RedisKey/redis_key.go
@@ -9,6 +9,11 @@ func RegisterVerificationCode(user string) string {
 	return "VerificationCode:Register:" + user
 }
 
+// ResetVerificationCode 重置密码用邮箱、手机验证码
+func ResetVerificationCode(user string) string {
+	return "VerificationCode:Reset:" + user
+}
+
 // UserToken 用户token
 func UserToken(userId int) string {
 	return "UserToken:" + strconv.Itoa(userId)
